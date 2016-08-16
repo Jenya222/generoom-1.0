f16e5e919ac7728bad42aed4b699fa5f7cfc3471
Беспалов Евгений Евгеньевич <E.Bespalov@kitnpf.int>
2016-08-16 14:13:23 +0300

    generoom 1.6

--- app/js/common.js
+++ app/js/common.js
@@ -81,14 +81,32 @@ $(document).ready(function() {
 });
   });
 
-$(document).ready(function() {
-    $("body").on("click", ".anch_goto", function(){
-    var idtop_goto = $($(this).attr("href")).offset().top;
-    $('html,body').animate({scrollTop: idtop_goto}, 1500);
-    return false;
-});
+//$(document).ready(function() {
+ //   $("body").on("click", ".anch_goto", function(){
+ //   var idtop_goto = $($(this).attr("href")).offset().top;
+ //   $('html,body').animate({scrollTop: idtop_goto}, 1500);
+ //   return false;
+//});
+//  });
+
+$(document).ready(function() {   
+var top_show = 150; // В каком положении полосы прокрутки начинать показ кнопки "Наверх"
+  var delay = 1000; // Задержка прокрутки
+  $(document).ready(function() {
+    $(window).scroll(function () { // При прокрутке попадаем в эту функцию
+      /* В зависимости от положения полосы прокрукти и значения top_show, скрываем или открываем кнопку "Наверх" */
+      if ($(this).scrollTop() > top_show) $('.anch_goto').fadeIn();
+      else $('.anch_goto').fadeOut();
+    });
+    $('.anch_goto').click(function () { // При клике по кнопке "Наверх" попадаем в эту функцию
+      /* Плавная прокрутка наверх */
+      $('body, html').animate({
+        scrollTop: 0
+      }, delay);
+    });
   });
-   
+});
+
 //counter
 $(document).ready(function() {
 $('.reviews>.wrapper').viewportChecker({
@@ -114,48 +132,7 @@ $('.reviews>.wrapper').viewportChecker({
 });
 
    });
-$('.reviews>.wrapper').scroll(function() {
-  $(body).css('background-color','red');
-var time = 4;
-$('.num').each(function(){
-  var i = 1,
-      num = $(this).data('num'),
-      step = 1000 * time / num,
-      that = $(this),
-      int = setInterval(function(){
-        if (i <= num) {
-          that.html(i);
-        }
-        else {
-          clearInterval(int);
-        }
-        i++;
-      },step);
-});
-  });
 
-ymaps.ready(init);
-var myMap, 
-    myPlacemark;
-
-function init(){ 
-    myMap = new ymaps.Map("map", {
-        center: [59.926938, 30.353089],
-        zoom: 17
-    }); 
-    
-    myPlacemark = new ymaps.Placemark([59.926938, 30.353089], {
-        balloonContent: 'Здесь квест астероид, чтобы записаться на него позвоните Арсению',
-        iconCaption: 'Я тута, пройди меня полностью'
-    },
-    {   hintContent: 'Квест астероид!',
-        preset: 'islands#greenDotIconWithCaption',
-        iconColor: '#fb383c'}
-    );
-
-    myMap.geoObjects.add(myPlacemark);
-
-        }
 
 //faq.html answer block
 $(document).ready(function(){
@@ -188,6 +165,27 @@ $(document).ready(function(){
           $(this).prev().css("display", "block")
           $(this).parent().parent().next().fadeOut(1000);
         });
+});
+//popups
+
+$(document).ready(function() {
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function(item) {
+        return item.el.attr('title') + '<small>by Generoom</small>';
+      }
+    }
+  });
 });
 
 $('.open-popup-link').magnificPopup({
@@ -245,3 +243,26 @@ $(document).ready(function(){
     });
 
 });
+
+ymaps.ready(init);
+var myMap, 
+    myPlacemark;
+
+function init(){ 
+    myMap = new ymaps.Map("map", {
+        center: [59.926938, 30.353089],
+        zoom: 17
+    }); 
+    
+    myPlacemark = new ymaps.Placemark([59.926938, 30.353089], {
+        balloonContent: 'Здесь квест астероид, чтобы записаться на него позвоните Арсению',
+        iconCaption: 'Я тута, пройди меня полностью'
+    },
+    {   hintContent: 'Квест астероид!',
+        preset: 'islands#greenDotIconWithCaption',
+        iconColor: '#fb383c'}
+    );
+
+    myMap.geoObjects.add(myPlacemark);
+
+        };
